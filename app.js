5d8e6dd568bc73972d783828c2aea6d4d06a2f08
1shefalika9 <shefalikag@outlook.com>
2021-07-18 14:19:44 +0530

    ready to try deploying

---
+++ app.js
@@ -0,0 +1,168 @@
+if (process.env.NODE_ENV !== "production") {
+	require('dotenv').config();
+}
+
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const MongoDBStore = require('connect-mongo');
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+	console.log("Database connected");
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
+
+const secret = process.env.SECRET || 'thishouldbeabettersecret!';
+
+const store = MongoDBStore.create({
+	mongoUrl: dbUrl,
+	secret: secret,
+	touchAfter: 24 * 60 * 60 //after 24 hrs if any changes to session only the update
+});
+
+store.on("error", function(e) {
+	console.log("SESSION STORE ERROR", e);
+})
+
+const sessionConfig = {
+	store,
+	name: 'session', //people know to look for connect.sid but changing the name can help a bit
+	secret,
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true, //cookie accessible only across http, cannot use JS to acess cookie
+		// secure : true, //i.e. cookie will only be made/configured over https
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7
+	}
+}
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(helmet());
+
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/"
+    // "https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js",
+    // "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/"
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/sg14/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session()); //has to come after session
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser()); //how to store a user in session
+passport.deserializeUser(User.deserializeUser()); ////how to un-store a user in session
+
+app.use((req, res, next) => {	
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
+})
+
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+
+app.get('/fakeUser', async (req, res) => {
+	const user = new User({email: 'sg@gmail.com', username: 'sg'});
+	const newUser = await User.register(user, 'friedchicken');
+	res.send(newUser);
+})
+
+app.get('/', (req, res) => {
+	res.render('home');
+})
+
+//for all requests, any path---order important
+app.all('*', (req, res, next) => {
+	next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+	const { statusCode = 500 } = err;
+	if (!err.message) {err.message = 'Something went wrong'}
+	res.status(statusCode).render('error', { err });
+})
+
+app.listen(3000, () => {
+	console.log('Serving on PORT 3000')
+})
